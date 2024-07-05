2f853a4a55713f36b98f786ba2c7a99dc8ed96ff
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-07-05 11:23:59 -0400

    Update MainActivity.java
    combine the 2 asynctask
    Catch the invalid mime type error and request the storage permission only when needed

--- app/src/main/java/name/lmj001/saveondevice/MainActivity.java
+++ app/src/main/java/name/lmj001/saveondevice/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends Activity {
     private Uri inputUri;
     private ArrayList<Uri> inputUris;
     private int currentFileIndex = 0;
-    private boolean saveIndividually;
+    private static boolean saveIndividually;
     private final static boolean supportsBuiltInAndroidFilePicker = Build.VERSION.SDK_INT>18;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,9 +89,6 @@ public class MainActivity extends Activity {
                     });
                 }
             } else {
-                if(Build.VERSION.SDK_INT>22 && Build.VERSION.SDK_INT<29 && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                }
                 tb.setChecked(saveIndividually);
                 tb.setOnCheckedChangeListener((buttonView, isChecked) -> settings.edit().putBoolean("saveIndividually", isChecked).apply());
             }
@@ -119,21 +116,6 @@ public class MainActivity extends Activity {
         });
     }
 
-    private String getMimeType(Uri uri) {
-        String mimeType = getApplicationContext().getContentResolver().getType(uri);
-
-        if (mimeType==null || mimeType.length()==0) {
-            String fileExtension = getOriginalFileName(this, uri);
-            fileExtension = fileExtension.substring(fileExtension.lastIndexOf('.') + 1).trim();
-            mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension.toLowerCase());
-            if (mimeType==null || mimeType.length()==0) mimeType = "application/octet-stream";
-        }
-        // It seems like some apps send the file with no MIME type on older Android versions. Possibly on newer versions the MIME type gets automatically set
-        // ContentResolver will refuse to open anything with invalid MIME type and the output file will be 0 bytes unless you grant WRITE_EXTERNAL_STORAGE.
-        // I am not sure if Google Play will allowing uploading the app with it but I guess I can put this if lmj isn't uploading it on Play Store anyway
-        return mimeType;
-    }
-
     private boolean isSupportedMimeTypes(ArrayList<Uri> uris) {
         for (Uri uri : uris) {
             String mimeType = getApplicationContext().getContentResolver().getType(uri);
@@ -144,22 +126,30 @@ public class MainActivity extends Activity {
 
     private void callSaveFileResultLauncherForIndividual(Uri inputUri, int code) {
         if (supportsBuiltInAndroidFilePicker) {
-            final String mimeType = getMimeType(inputUri);
-
-            if (mimeType.isEmpty()) {
-                Toast.makeText(getApplicationContext(), R.string.unsupported_mimetype, Toast.LENGTH_LONG).show();
-                finish();
-            } else {
-                Intent saveFileIntent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-                saveFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
-                saveFileIntent.setType(mimeType);
-                saveFileIntent.putExtra(Intent.EXTRA_TITLE, getOriginalFileName(this, inputUri));
-                startActivityForResult(saveFileIntent, code);
+            String mimeType;
+            try {
+                mimeType = getApplicationContext().getContentResolver().getType(inputUri);
+            } catch (NullPointerException ignored) {
+                // It seems like some apps send the file with no MIME type on older Android versions. Possibly on newer versions the MIME type gets automatically set
+                // ContentResolver will refuse to open anything with invalid MIME type and the output file will be 0 bytes unless you grant WRITE_EXTERNAL_STORAGE.
+                // I am not sure if Google Play will allowing uploading the app with it but I guess I can put this if lmj isn't uploading it on Play Store anyway
+                if(Build.VERSION.SDK_INT>22 && Build.VERSION.SDK_INT<29 && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                }
+                String fileExtension = getOriginalFileName(this, inputUri);
+                fileExtension = fileExtension.substring(fileExtension.lastIndexOf('.') + 1).trim();
+                mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension.toLowerCase());
+                if (mimeType==null || mimeType.isEmpty()) mimeType = "application/octet-stream";
             }
+            Intent saveFileIntent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+            saveFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+            saveFileIntent.setType(mimeType);
+            saveFileIntent.putExtra(Intent.EXTRA_TITLE, getOriginalFileName(this, inputUri));
+            startActivityForResult(saveFileIntent, code);
         } else {
             Uri outputUri = Uri.fromFile(new File(getSharedPreferences("set", Context.MODE_PRIVATE).getString("directoryToSaveFiles", Environment.getExternalStorageDirectory().getPath() + File.separator + "Download") + getOriginalFileName(this, inputUri)));
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
-                new SaveFileAsyncTask(this).execute(inputUri, outputUri);
+                new SaveFileTask(this).execute(inputUri, outputUri);
             } else {
                 saveFile(inputUri, outputUri);
             }
@@ -230,8 +220,7 @@ public class MainActivity extends Activity {
             finish();
         }
     }
-
-
+    
     private String getOriginalFileName(Context context, Uri uri) {
         String result = null;
         try {
@@ -285,20 +274,17 @@ public class MainActivity extends Activity {
                 switch (requestCode) {
                     case SAVE_FILE_REQUEST_CODE:
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
-                            new SaveFileAsyncTask(this).execute(inputUri, outputUri);
+                            new SaveFileTask(this).execute(inputUri, outputUri);
                         } else {
                             saveFile(inputUri, outputUri);
                         }
                         finish();
                         break;
                     case SAVE_FILES_REQUEST_CODE:
-                        if(saveIndividually || Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
-                                new SaveFileAsyncTask(this).execute(inputUri, outputUri);
-                            } else {
-                                saveFile(inputUri, outputUri);
-                            }
-                            // If there are more input URIs to process, save the next file; otherwise, finish the activity
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+                            new SaveFileTask(this).execute(inputUri, outputUri);
+                        } else {
+                            saveFile(inputUri, outputUri);
                             if (inputUris != null && currentFileIndex < inputUris.size() - 1) {
                                 currentFileIndex++;
                                 inputUri = inputUris.get(currentFileIndex);
@@ -306,9 +292,6 @@ public class MainActivity extends Activity {
                             } else {
                                 finish();
                             }
-                        } else {
-                            new SaveMultipleFilesAsyncTask(this).execute(outputUri);
-                            finish();
                         }
                         break;
                 }
@@ -322,58 +305,49 @@ public class MainActivity extends Activity {
         super.finish();
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private static class SaveMultipleFilesAsyncTask extends AsyncTask<Uri, Void, Void> {
+    @TargetApi(Build.VERSION_CODES.CUPCAKE)
+    private static class SaveFileTask extends AsyncTask<Uri, Void, Void> {
         private final WeakReference<MainActivity> activityReference;
-        // only retain a weak reference to the activity
 
-        public SaveMultipleFilesAsyncTask(MainActivity context) {
+        public SaveFileTask(MainActivity context) {
             this.activityReference = new WeakReference<>(context);
         }
 
-        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
         @Override
         protected Void doInBackground(Uri... uris) {
             MainActivity activity = activityReference.get();
-            final Uri treeUri = uris[0];
-            if (treeUri != null) {
-                ContentResolver resolver = activity.getContentResolver();
-                try {
-                    Uri docUri = DocumentsContract.buildDocumentUriUsingTree(treeUri, DocumentsContract.getTreeDocumentId(treeUri));
-                    for (Uri inputUri : activity.inputUris) {
-                        String fromUriFileName = activity.getOriginalFileName(activity, inputUri);
-                        Uri fileUri = DocumentsContract.createDocument(resolver, docUri, "*/*", fromUriFileName);
-                        try (InputStream inputStream = resolver.openInputStream(inputUri);
-                             FileOutputStream outputStream = (FileOutputStream) resolver.openOutputStream(fileUri)){
-                           byte[] buffer = new byte[4096];
-                           int length;
-                           while ((length = inputStream.read(buffer)) > 0) {
-                               outputStream.write(buffer, 0, length);
-                           }
-                       }
+            if (activity == null) return null;
 
+            if (saveIndividually || Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                activity.saveFile(uris[0], uris[1]);
+                if (activity.inputUris != null && activity.currentFileIndex < activity.inputUris.size() - 1) {
+                    activity.currentFileIndex++;
+                    activity.inputUri = activity.inputUris.get(activity.currentFileIndex);
+                    activity.callSaveFileResultLauncherForIndividual(activity.inputUri, SAVE_FILES_REQUEST_CODE);
+                }
+            } else {
+                final Uri treeUri = uris[1];
+                if (treeUri != null) {
+                    ContentResolver resolver = activity.getContentResolver();
+                    try {
+                        Uri docUri = DocumentsContract.buildDocumentUriUsingTree(treeUri, DocumentsContract.getTreeDocumentId(treeUri));
+                        for (Uri inputUri : activity.inputUris) {
+                            String fromUriFileName = activity.getOriginalFileName(activity, inputUri);
+                            Uri fileUri = DocumentsContract.createDocument(resolver, docUri, "*/*", fromUriFileName);
+                            try (InputStream inputStream = resolver.openInputStream(inputUri);
+                                 OutputStream outputStream = resolver.openOutputStream(fileUri)) {
+                                byte[] buffer = new byte[4096];
+                                int length;
+                                while ((length = inputStream.read(buffer)) > 0) {
+                                    outputStream.write(buffer, 0, length);
+                                }
+                            }
+                        }
+                    } catch (Exception e) {
+                        activity.showError(e.toString());
                     }
-                } catch (Exception e) {
-                    activity.showError(e.toString());
                 }
             }
-            return null;
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.CUPCAKE)
-    private static class SaveFileAsyncTask extends AsyncTask<Uri, Void, Void> {
-        private final WeakReference<MainActivity> activityReference;
-        // only retain a weak reference to the activity
-
-        public SaveFileAsyncTask(MainActivity context) {
-            this.activityReference = new WeakReference<>(context);
-        }
-
-        @Override
-        protected Void doInBackground(Uri... uris) {
-            MainActivity activity = activityReference.get();
-            activity.saveFile(uris[0], uris[1]);
             return null;
         }
     }
